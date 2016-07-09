35f72cf2390dba95547ff58b94bfd91adedb5b61
robmill <r_miller213@hotmail.com>
2016-07-09 09:09:14 -0700

    Third Plot

---
+++ plot3.R
@@ -0,0 +1,49 @@
+# Rob Miller
+# plot3.R
+# Course Project 1
+
+
+# read data from temp directory
+df<-read.table("c:/temp/household_power_consumption.txt",sep = ";",header = TRUE,stringsAsFactors = FALSE)
+
+# create new variable combining Date and Time columns
+# reformat
+# extract day names and assign to new variable
+df$DateTime<-paste(df$Date,df$Time)
+df$DateTime<-strptime(df$DateTime,format = "%d/%m/%Y %H:%M:%S",tz="")
+df$days<-weekdays(df$DateTime)
+
+# reassign classes of variables
+df$Date<-as.Date(df$Date,"%d/%m/%Y");
+df$Global_active_power<-as.numeric(df$Global_active_power)
+df$Global_reactive_power<-as.numeric(df$Global_reactive_power)
+df$Voltage<-as.numeric(df$Voltage)
+df$Global_intensity<-as.numeric(df$Voltage)
+df$Sub_metering_1<-as.numeric(df$Sub_metering_1)
+df$Sub_metering_2<-as.numeric(df$Sub_metering_2)
+df$Sub_metering_3<-as.numeric(df$Sub_metering_3)
+
+# set start and end dates
+df_start<-as.POSIXlt("2007-02-01 00:00:00")
+df_end<-as.POSIXlt("2007-02-03 00:00:00")
+
+
+# subset dataset between start and end dates
+df_subset<-subset(df, df$DateTime>=df_start & df$DateTime<=df_end)
+
+# extract weekay names for x-axis
+x_axis_days<-unique(df_subset$day)
+
+#plot Gloval Active Power
+#plot(y=df_subset$Global_active_power,x=df_subset$DateTime,type="l",xlab=n)
+#title(ylab="Global Active Power (kilowatts)",xlab=NULL)
+plot(y=df_subset$Sub_metering_1,x=df_subset$DateTime, type="l",xlab=n)
+lines(y=df_subset$Sub_metering_2,x=df_subset$DateTime,col="red",type="l")
+lines(y=df_subset$Sub_metering_3,x=df_subset$DateTime,col="blue",type="l")
+
+# apply legend
+legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),lty=c(1,1,1),col=c("black","red","blue"))
+
+
+dev.copy(png,file="plot.3.png")
+dev.off()
